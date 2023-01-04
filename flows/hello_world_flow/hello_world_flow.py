ab7d409f13fcaca1ba4dbf146d62bf7d0e9bf50b
Taylor Curran <taylorcurranagtc@gmail.com>
2023-01-04 08:09:37 -0500

    adding terraform files

--- flows/hello_world_flow/hello_world_flow.py
+++ flows/hello_world_flow/hello_world_flow.py
@@ -1,12 +1,17 @@
 from prefect import flow, task
+from prefect_dask.task_runners import DaskTaskRunner
 
 
 @task
 def my_task():
     print("Hello World")
 
+my_dask_task_runner = DaskTaskRunner(
+    cluster_class="dask_cloudprovider.aws.FargateCluster",
+    adapt_kwargs={"maximum": 10}
+)
 
-@flow
+@flow(task_runner=DaskTaskRunner)
 def hello_world():
     my_task()
     print("What is your favorite number?")
--- terraform/ecs_agent/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "prefect_agent_service_id" {
-  value = aws_ecs_service.prefect_agent_service.id
-}
