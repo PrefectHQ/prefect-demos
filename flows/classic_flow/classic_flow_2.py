14698c3c0a8834b0861f964dd73b7f213261bd3a
Taylor Curran <taylorcurranagtc@gmail.com>
2023-01-05 19:56:41 -0500

    added list_objs task and looping

--- flows/classic_flow/classic_flow_2.py
+++ flows/classic_flow/classic_flow_2.py
@@ -1,6 +1,7 @@
 from re import L
-from prefect import task, flow
+from prefect import flow, task, unmapped
 from prefect_aws.s3 import S3Bucket
+from datetime import date, timedelta
 from prefect.orion.schemas.states import Completed, Failed
 from prefect.blocks.notifications import SlackWebhook
 from prefect.tasks import task_input_hash
@@ -11,25 +12,46 @@ import random
 import pandas as pd
 from io import StringIO
 
+# subflows
+# looping
+# if statement
+# run_deployment
+# query snowflake (1 simple pre-built task)
 
 @task
-def read_in_raw_data():
+def list_s3_objects(s3_block_raw_data: S3Bucket):
+    obj_dict = s3_block_raw_data.list_objects()
+    objs = [obj_dict[i]["Key"] for i in range(len(obj_dict))]
 
-    s3_raw_data_bucket = S3Bucket.load("raw-data-jaffle-shop")
-    csv_contents = s3_raw_data_bucket.read_path("jaffle_shop_customers.csv")
-    df = pd.read_csv(StringIO(csv_contents.decode("utf-8")))
+    return objs
+
+@task
+def read_csv_to_df(s3_block_raw_data: S3Bucket, object_key):
+
+    csv = s3_block_raw_data.read_path(object_key)
+    df = pd.read_csv(StringIO(csv.decode("utf-8")))
 
     return df
 
+@task
 def get_row_count(df):
-    print(df.shape[0])
+    print(df.shape)
 
-@flow(retries=2, retry_delay_seconds=30)
+@flow()#retries=2, retry_delay_seconds=30)
 def main_flow(
         start_date: date = date(2020, 2, 1),
         end_date: date = date.today(),
+        risk_level: int = 0
 ):
-    read_in_raw_data()
+    # Load in Block to Instantiate Block Object
+    s3_block_raw_data = S3Bucket.load("raw-data-jaffle-shop")
+
+    # First Task
+    s3_objs = list_s3_objects(s3_block_raw_data)
+
+    dfs = {}
+    for i in range(len(s3_objs)):
+        dfs.update({s3_objs[i].rstrip('.csv'): read_csv_to_df(s3_block_raw_data, s3_objs[i])})
 
 
 if __name__ == "__main__":
